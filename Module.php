71a280eaf7f74efa6846a49e5ff5376ee4d68c4e
غلامرضا بهشتیان <amintado@gmail.com>
2017-09-11 23:17:55 +0430

    create module files

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace amintado\slider;
+
+/**
+ * slide module definition class
+ */
+class module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'amintado\slider\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
